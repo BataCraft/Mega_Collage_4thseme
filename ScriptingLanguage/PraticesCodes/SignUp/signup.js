2e99ace7618e2c2c2fa101f24945cc38dac4a002
saroj bata <lordhunter017@gmail.com>
2023-12-07 07:44:12 +0545

    change and add files

--- ScriptingLanguage/PraticesCodes/SignUp/signup.js
+++ ScriptingLanguage/PraticesCodes/SignUp/signup.js
@@ -8,9 +8,30 @@ form.addEventListener("submit", function(e){
     e.preventDefault();
     }
     if(email.value == ''){
-        console.log("plese fill up email");
-        e.preventDefault();
-    }
-    // console.log();
-});
+        // console.log("plese fill up email");
+        // e.preventDefault();
 
+        
+        email.addEventListener("keyup", function(){
+            console.log(this.value);
+            if(this.value.indexOf("@") == -1)
+            {
+                console.log(`At least '@' sy,bol is required.`);
+            }
+            else if (this.value.indexOf("@") != this.value.lastIndexOf("@"))
+            {
+                console.log(`At least one '@' Symbol is not allowed.`);
+            }
+            else if (this.value.indexOf(".") != -1)
+            {
+                console.log(`At least one '.' Symbol is required`);
+            }
+            else{
+                console.log(`OK`);
+            }
+        })
+    }
+    });
+
+console.log(Array());
+console.log(Object());
