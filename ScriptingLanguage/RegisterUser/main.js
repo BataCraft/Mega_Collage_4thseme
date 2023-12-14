922aacb9f2dc87ef2f859fbf6003c2cfae863970
saroj bata <lordhunter017@gmail.com>
2023-12-14 07:37:58 +0545

    add

---
+++ ScriptingLanguage/RegisterUser/main.js
@@ -0,0 +1,17 @@
+// let form = document.register;
+// let fullname = fullname,
+// email = email,
+// psd = password;
+
+// form.eventListiner("submit", function(){
+//     if(fullname.value == ""){
+//         firstname.nextElementSibiling.innerText = "Your Full name is Empty"
+//     }
+// })
+
+
+
+let date = new Date
+let day = date.getDay() +1,
+year = date.getFullYear();
+console.log(year);
